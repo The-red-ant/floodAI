e2db9ad044aee5c966eea59310e3ffd17b3a98da
Anthony Sinchi <anthonysinchi655@gmial.com>
2024-11-03 01:46:46 -0400

    mocup(sorta a)

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const map = L.map('map').setView([51.505, -0.09], 13); // Example coordinates (L
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+// define rectangle geographical bounds
+var bounds = [[54.559322, -5.767822], [56.1210604, -3.021240]];
+
+// create an orange rectangle
+L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
+
+// zoom the map to the rectangle bounds
+map.fitBounds(bounds);
